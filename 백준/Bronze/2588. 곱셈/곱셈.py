148ee4ffbacac3fd30e0624dd479b18107502f20
I-LOVE-JAZZ <coding_jazzman@naver.com>
2025-01-27 16:34:12 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,6 +1,4 @@
-A = int(input())
-B = input()
-print(A * int(B[2]))
-print(A * int(B[1]))
-print(A * int(B[0]))
-print(A * int(B))
+a = int(input())
+b2, b1, b0 = input().strip()
+r = (r0 := a * int(b0)) + (r1 := a * int(b1)) * 10 + (r2 :=a * int(b2)) * 100
+print(r0, r1, r2, r, sep='\n')
