1893783feac411bd80cab4b719ef35d72ff6d11d
codingorijazz <coding_jazzman@naver.com>
2025-03-11 22:01:25 +0900

    33

--- .vscode/test/test.py
+++ .vscode/test/test.py
@@ -1,11 +1,33 @@
-N = int(input())
-for i in range(1, N+1):
-    print("*" * i)
+import sys
 
-    
-
-
-
+data = sys.stdin.read().splitlines()
+for line in data:
+    a, b = map(int, line.split())
+    print(a + b)
 
 
 
+
+
+
+
+
+
+
+# import sys
+
+# for i in sys.stdin:
+#     A, B = map(int,i.split())
+#     print(A+B)
+
+
+
+
+
+
+# while True:
+#     try:
+#         A,B = map(int,input().split())
+#         print(A+B)
+#     except:
+#         break"
